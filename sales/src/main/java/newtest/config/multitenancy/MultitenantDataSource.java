5a60f129371e2f70980a04cc0bc433620094934a
Jinyoung Jang <jyjang@uengine.org>
2023-07-31 10:19:22 +0000

    multi-tenancy support added

---
+++ sales/src/main/java/newtest/config/multitenancy/MultitenantDataSource.java
@@ -0,0 +1,11 @@
+package newtest.config.multitenancy;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+public class MultitenantDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected String determineCurrentLookupKey() {
+        return TenantContext.getCurrentTenant();
+    }
+}
